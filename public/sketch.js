05f077c1686c7e81d6239400a514ebc4666cbf53
Curime <curime.batliner@gmail.com>
2021-09-11 14:48:56 -0700

    another commit3

--- public/sketch.js
+++ public/sketch.js
@@ -1,21 +1,51 @@
-//BUILDING WEB-BASED DRAWING INSTRUMENTS TEMPLATE
-//BY GALO CANIZARES
+var myArray = [];
+var trailLength = 25;
+var counter = 0;
 
 function setup() {
-  pixelDensity(1);
+  createCanvas(500, 500);
+  frameRate(30);
 
-  canvasIMG = createCanvas(window.windowWidth, window.windowHeight);
-  background(255, 0, 0);
+  for (var i=0; i<5;i++){
+    //myArray.push(new smileyFace(random(400),random(400),random(50,100)));
+  }
+  smileyFace(mouseX,mouseY);
 
 }
 
 function draw() {
-  //background(bgr, bgg, bgb, alph);
-  ellipse(mouseX, mouseY, 25, 25);
+  background(220);
+  for (var i =0 ; i< myArray.length;i++){
+    myArray[i].show(i);
+  }
+  if (myArray.length>trailLength){
+    myArray.splice(0,1);
+
+  }
+  textAlign(CENTER);
+  textSize(height/20);
+  text("BE HAPPY MY FRIEND",width/2,height/2)
+  console.log(counter);
+}
+
+function mouseDragged(){
+  myArray.push(new smileyFace(mouseX,mouseY, random(25,50)));
 
 }
 
-function windowResized() {
-  resizeCanvas(window.windowWidth, window.windowHeight);
-  resetCanvas();
+function smileyFace(x,y,diameter){
+  this.x = x;
+  this.y = y;
+  this.d = diameter;
+  this.show = function(variable){
+  fill(sin(variable),cos(variable),255,variable);
+  ellipse(this.x,this.y,this.d,this.d);
+  fill(variable);
+  ellipse(this.x+this.d/10, this.y-this.d/10,this.d/10);
+  ellipse(this.x-this.d/10, this.y-this.d/10,this.d/10);
+  rectMode(CENTER);
+  rect(this.x,this.y+this.d/5,this.d/3,this.d/10);
+
+  }
+
 }
